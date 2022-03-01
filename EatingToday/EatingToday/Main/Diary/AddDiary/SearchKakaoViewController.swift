003f8b6ef15fca6c59385f5cc163abb5c0949226
seonghooony <sh-k@kakao.com>
2022-03-01 20:53:19 +0900

    add adddiaryviewcontroller

---
+++ EatingToday/EatingToday/Main/Diary/AddDiary/SearchKakaoViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SearchKakaoViewController.swift
+//  EatingToday
+//
+//  Created by SeongHoon Kim on 2022/03/01.
+//
+
+import Foundation
---
+++ EatingToday/EatingToday/Main/Diary/AddDiary/SearchResultOverview.swift
@@ -0,0 +1,8 @@
+//
+//  SearchResultOverview.swift
+//  EatingToday
+//
+//  Created by SeongHoon Kim on 2022/03/01.
+//
+
+import Foundation
